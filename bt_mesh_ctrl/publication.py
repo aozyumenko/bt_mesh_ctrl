40e637f5c4b0ffd174e06bc04f8e88d6f0414b3d
Alexander Ozumenko <scg@stdio.ru>
2026-08-26 11:38:35 +0300

    cleanup publication.py

--- bt_mesh_ctrl/publication.py
+++ bt_mesh_ctrl/publication.py
@@ -2,6 +2,9 @@
 from __future__ import annotations
 
 
+from construct import Container
+
+
 __all__ = (
     "Publication",
 )
@@ -10,17 +13,14 @@ __all__ = (
 class Publication:
     @classmethod
     def extract(_class, status: Container) -> dict():
-#        try:
-            publication = {
-                "unicast_addr": f"0x{status.publication_address:04x}",
-                "app_key": status.app_key_index,
-                "ttl": status.ttl,
-                "period": status.period,
-                "retransmissions": {
-                    "count": status.retransmissions["count"],
-                    "interval": status.retransmissions["interval"]
-                }
+        publication = {
+            "unicast_addr": f"0x{status.publication_address:04x}",
+            "app_key": status.app_key_index,
+            "ttl": status.ttl,
+            "period": status.period,
+            "retransmissions": {
+                "count": status.retransmissions["count"],
+                "interval": status.retransmissions["interval"]
             }
-            return publication
-#        except AttributeError as e:
-#            return {}
+        }
+        return publication
