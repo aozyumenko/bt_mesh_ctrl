fd614c393931a5e4d54bff15ab8048ef764a129b
Alexander Ozumenko <scg@stdio.ru>
2026-08-26 11:31:49 +0300

    cleanup mesh_provisioner_conf.py

--- bt_mesh_ctrl/mesh_provisioner_conf.py
+++ bt_mesh_ctrl/mesh_provisioner_conf.py
@@ -1,11 +1,11 @@
 """mesh_cfgclient provisioner configuration file reader"""
 from __future__ import annotations
 
-import os.path, time
+import os.path
 from pathlib import Path
-import itertools
 import json
 from dataclasses import dataclass
+from typing import Final
 from uuid import UUID
 
 from bluetooth_mesh.messages.config import GATTNamespaceDescriptor
@@ -19,8 +19,6 @@ _LOGGER = logging.getLogger(__name__)
 __all__ = (
     "MeshProvisionerConf"
 )
-
-
 
 
 JSON_TOKEN: Final = "token"
@@ -48,6 +46,7 @@ class MeshProvisionerElement:
     models: list[BtMeshModelId]
     location: GATTNamespaceDescriptor
 
+
 @dataclass
 class MeshProvisioner:
     uuid: UUID
@@ -66,11 +65,15 @@ class MeshProvisionerConf:
 
     def _parse(self, data):
         # get all allowing for network AppKeys
-        self._appkeys = [appkey[JSON_INDEX] for appkey in data.get(JSON_APP_KEYS, ())]
+        self._appkeys = [
+            appkey[JSON_INDEX] for appkey in data.get(JSON_APP_KEYS, ())
+        ]
 
         # get provisioners
-        provisioners = [provisioner[JSON_UUID]
-            for provisioner in data.get(JSON_PROVISIONERS, ())]
+        provisioners = [
+            provisioner[JSON_UUID]
+            for provisioner in data.get(JSON_PROVISIONERS, ())
+        ]
 
         # enumerate nodes and search provisioners
         for node in data.get(JSON_NODES, ()):
@@ -80,15 +83,18 @@ class MeshProvisionerConf:
 
                 self._nodes_unicast_addr[node_uuid] = node_unicast_addr
 
-                node_net_keys = [int(net_key[JSON_INDEX]) for net_key in node[JSON_NET_KEYS] if not net_key[JSON_UPDATED]]
+                node_net_keys = [
+                    int(net_key[JSON_INDEX])
+                    for net_key in node[JSON_NET_KEYS]
+                    if not net_key[JSON_UPDATED]
+                ]
                 node_elements = node[JSON_ELEMENTS]
                 node_cid = int(node[JSON_CID], 16)
                 node_pid = int(node[JSON_PID], 16)
                 node_vid = int(node[JSON_VID], 16)
                 node_crpl = int(node[JSON_CRPL], 16)
-            except Exception as e:
+            except Exception:
                 continue
-
 
             if node_uuid not in provisioners:
                 continue
@@ -100,7 +106,7 @@ class MeshProvisionerConf:
                     element_index = int(element[JSON_INDEX])
                     element_location = int(element[JSON_LOCATION], 16)
                     element_models = element[JSON_MODELS]
-                except Exception as e:
+                except Exception:
                     continue
 
                 # get element's models
@@ -108,30 +114,30 @@ class MeshProvisionerConf:
                 for model in element_models:
                     try:
                         model_id = int(model[JSON_MODEL_ID], 16)
-                    except Exception as e:
+                    except Exception:
                         continue
                     provisioner_element_models.append(
                         BtMeshModelId(model_id)
                     )
 
                 provisioner_element = MeshProvisionerElement(
-                    index = element_index,
-                    location = GATTNamespaceDescriptor(element_location),
-                    models = provisioner_element_models
+                    index=element_index,
+                    location=GATTNamespaceDescriptor(element_location),
+                    models=provisioner_element_models
                 )
                 provisioner_elements.insert(element_index, provisioner_element)
 
         provisioner = MeshProvisioner(
-            uuid = node_uuid,
-            cid = node_cid,
-            pid = node_pid,
-            vid = node_vid,
-            crpl = node_crpl,
-            elements = provisioner_elements
+            uuid=node_uuid,
+            node_net_keys=node_net_keys,
+            cid=node_cid,
+            pid=node_pid,
+            vid=node_vid,
+            crpl=node_crpl,
+            elements=provisioner_elements
         )
 
         self._provisioners.append(provisioner)
-
 
     def __init__(self, filename: str):
         self.filename: str = os.path.expanduser(filename)
@@ -154,10 +160,10 @@ class MeshProvisionerConf:
     def token(self) -> int:
         return self._token
 
-    def get_provisioners_num(self) -> MeshCfgProvisioner:
+    def get_provisioners_num(self) -> int:
         return len(self._provisioners)
 
-    def get_provisioner(self, idx: int) -> MeshCfgProvisioner:
+    def get_provisioner(self, idx: int) -> MeshProvisioner:
         return self._provisioners[idx]
 
     def get_node_unicast_addr(self, uuid: str):
